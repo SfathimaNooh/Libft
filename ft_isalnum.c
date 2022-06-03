0efa2240acb5cee8b8043c83d7a1d877806c5954
Sumaiya Fathima <106827903+SfathimaNooh@users.noreply.github.com>
2022-06-03 20:12:03 +0400

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sfathima <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sfathima <sfathima@student.42abudhabi      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:35:55 by sfathima          #+#    #+#             */
 /*   Updated: 2021/10/03 15:13:15 by sfathima         ###   ########.fr       */
